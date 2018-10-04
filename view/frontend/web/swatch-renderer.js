1412ea3ec22c71911b6dfd5c09249af0045bec12
Dmitry Fedyuk <dfediuk@gmail.com>
2018-10-05 00:31:08 +0600

    1.4.0: https://github.com/frugue/core/issues/26

--- view/frontend/web/swatch-renderer.js
+++ view/frontend/web/swatch-renderer.js
@@ -1,16 +1,36 @@
-// 2018-08-02
-// «Preselect the first color swatch for configurable products»
-// https://github.com/frugue/configurable/issues/3
-// https://www.upwork.com/ab/f/contracts/20488254
 define([
 	'df-lodash', 'jquery', 'mage/utils/wrapper'
-], function(_, $, w) {'use strict'; return function(sb) {
+], function(_, $, w) {'use strict';
+return function(sb) {
+var ids;
 $.extend(sb.prototype, {
 	_getSelectedAttributes: w.wrap(sb.prototype._getSelectedAttributes, function(_super) {
 		var r = _super();
 		var k = 'color_lg';
-		if (!r[k]) {
+		// 2018-08-02
+		// «Preselect the first color swatch for configurable products»:
+		// https://github.com/frugue/configurable/issues/3
+		// https://www.upwork.com/ab/f/contracts/20488254
+		if (!$('body').hasClass('page-products') && !r[k]) {
 			r[k] = $('.' + k + ' .swatch-option', this.element).first().attr('option-id');
+		}
+		// 2018-08-02
+		// «Preselect the first color swatch for configurable products»:
+		// https://github.com/frugue/configurable/issues/3
+		// https://www.upwork.com/ab/f/contracts/20488254
+		else {
+			ids = ids || $('.filter-options .swatch-layered.color_lg .swatch-option.selected').map(function() {
+				return $(this).attr('option-id');
+			}).get();
+			if (ids.length) {
+				var $o = $('.swatch-option', this.element);
+				var id = _.find(ids, function(id) {
+					return $o.filter(function() {return id === $(this).attr('option-id');}).length;
+				});
+				if (id) {
+					r[k] = id;
+				}
+			}
 		}
 		return r;
 	})
